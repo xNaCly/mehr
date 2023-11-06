7c50632353d7a1cbe4ce2c2a63b66b2d63adea30
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-11-06 12:04:05 +0100

    config: cache, validation
    This commit introduces caching for configuration, therefore omitting the
    need to read and parse the configuration file multiple times.

--- config/config.go
+++ config/config.go
@@ -1,12 +1,29 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
+	_ "embed"
+
 	"github.com/BurntSushi/toml"
+	"github.com/xnacly/mehr/pkgmgr"
+	"github.com/xnacly/mehr/types"
 )
+
+type cache struct {
+	config *types.Configuration
+	path   string
+}
+
+// only filled once, afterwards used to cache the configuration - this allows
+// us to skip file reading and toml decoding
+var configCache *cache = &cache{}
+
+//go:embed mehr.toml
+var DefaultConfigFileContent []byte
 
 // returns path the mehr config is located
 func LookUp() string {
@@ -15,16 +32,45 @@ func LookUp() string {
 }
 
 // reads configuration at 'path', decodes to Configuration struct
-func Get(path string) (*Configuration, error) {
-	c := &Configuration{}
-	c.Ok()
+func Get(path string) (*types.Configuration, error) {
+	if configCache != nil && configCache.path == path {
+		return configCache.config, nil
+	}
+	c, err := ValidateConfig(types.Configuration{})
+	if err != nil {
+		return c, err
+	}
 	out, err := os.ReadFile(path)
 	if err != nil {
-		return c, fmt.Errorf("Failed to read from configuration file: %w", err)
+		return c, fmt.Errorf("Failed to read configuration file, using fallback: %w", err)
 	}
 	_, err = toml.Decode(string(out), c)
 	if err != nil {
 		return c, fmt.Errorf("Failed to decode configuration file: %w", err)
 	}
+	configCache.config = c
+	configCache.path = path
 	return c, nil
+}
+
+// validates the struct and fills empty fields
+func ValidateConfig(c types.Configuration) (*types.Configuration, error) {
+	if c.PackageManager == "" || c.PackageManager == "auto" {
+		if mgr, ok := pkgmgr.Get(); ok {
+			c.PackageManager = mgr.Name
+		} else {
+			return nil, errors.New("No package manager found")
+		}
+	}
+	if c.SystemConfig == nil {
+		c.SystemConfig = &types.SystemConfig{}
+	}
+	if c.SystemConfig.Path == "" {
+		confHome, err := os.UserConfigDir()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get user config dir: %w", err)
+		}
+		c.SystemConfig.Path = confHome
+	}
+	return &c, nil
 }
