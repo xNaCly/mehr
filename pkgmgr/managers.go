b706f4374623954912f297ed27602c82c6cfbc03
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-11-06 12:05:40 +0100

    pkgmgr: renamed package

---
+++ pkgmgr/managers.go
@@ -0,0 +1,54 @@
+package pkgmgr
+
+import (
+	"fmt"
+
+	"github.com/xnacly/mehr/types"
+)
+
+var Managers = []*PackageManager{
+	{
+		Name:    "apt",
+		options: []string{"-y"}, // skips [Y/n] confirmation prompts
+		install: &types.SubCommand{Name: "install"},
+		update:  &types.SubCommand{Name: "update"},
+		upgrade: &types.SubCommand{Name: "upgrade"},
+		remove:  &types.SubCommand{Name: "remove"},
+	},
+	{
+		Name:    "pacman",
+		install: &types.SubCommand{Name: "-S", Options: []string{"--noconfirm"}},
+		update:  &types.SubCommand{Name: "-Sy"},
+		upgrade: &types.SubCommand{Name: "-Su", Options: []string{"--noconfirm"}},
+		remove:  &types.SubCommand{Name: "-Rs", Options: []string{"--noconfirm"}},
+		formatPkgWithVersion: func(name, version string) string {
+			return name + "=" + version
+		},
+	},
+}
+
+// returns the package manager for name if found, otherwise errors
+func GetByName(name string) (*PackageManager, error) {
+	for _, mgr := range Managers {
+		if mgr.Name == name {
+			path, ok := mgr.Exists()
+			if !ok {
+				return nil, fmt.Errorf("Package manager %q not found on operating system", mgr.Name)
+			}
+			mgr.Name = path
+			return mgr, nil
+		}
+	}
+	return nil, fmt.Errorf("Package manager %q not found in defined list of package managers", name)
+}
+
+// returns the currently available package manager on the system
+func Get() (*PackageManager, bool) {
+	for _, mgr := range Managers {
+		if path, ok := mgr.Exists(); ok {
+			mgr.Name = path
+			return mgr, true
+		}
+	}
+	return nil, false
+}
---
+++ types/types.go
@@ -0,0 +1,44 @@
+package types
+
+// configure a package
+type PackageConfig struct {
+	Content string `toml:"content"` // content to write to configuration file
+	URL     string `toml:"url"`     // location of configuration to write to configuration file
+	Path    string `toml:"path"`    // location of the file contents to copy to the configuration file
+	Link    string `toml:"link"`    // location of the file to link the configuration file to
+}
+
+type SystemConfig struct {
+	// path to store package configuration at, falls back to `$XDG_CONFIG_HOME` for
+	// linux, %AppData% for windows and $HOME/Library/Application Support/ for
+	// macOS
+	Path string `toml:"path"`
+	// specify configuration files from various sources
+	Files map[string]*PackageConfig `toml:"files"`
+}
+
+// install a singular package
+type Package struct {
+	Version string `toml:"version"` // leave empty for latest
+	URL     string `toml:"url"`     // url to download package from
+}
+
+type Configuration struct {
+	PackageManager string              `toml:"package-manager"` // specify what package manager to use, path or empty for auto lookup
+	Packages       map[string]*Package `toml:"packages"`        // packages to install
+	SystemConfig   *SystemConfig       `toml:"config"`          // configure the system and the installed packages
+}
+
+type Manager interface {
+	Install(packages []Package) error
+	Upgrade(packages []string) error
+	Remove(packages []string) error
+	Exists() bool
+	Update() error
+	Output() string
+}
+
+type SubCommand struct {
+	Name    string
+	Options []string
+}
